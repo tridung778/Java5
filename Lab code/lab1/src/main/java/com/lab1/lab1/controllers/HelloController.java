f3c9380e6d1e6b60bbf99aa3fbba410b17868ee3
tridung778 <crytv778@gmail.com>
2024-04-17 17:38:44 +0700

    dung comit

---
+++ Lab code/lab1/src/main/java/com/lab1/lab1/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.lab1.lab1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("")
+    public String index() {
+        return "index";
+    }
+}
