d70f51da01abaf68ec7132582640cceb871c192f
tridung778 <crytv778@gmail.com>
2024-04-24 18:04:26 +0700

    4/24

---
+++ Source Code Tutorial/RetailStore/src/main/java/com/example/RetailStore/RetailStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.RetailStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailStoreApplication.class, args);
+	}
+
+}
---
+++ Source Code Tutorial/RetailStore/src/test/java/com/example/RetailStore/RetailStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RetailStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
